2ab7e6c76956134c8ecd9e99ddcc36388061c32a
James Morris <jamesramorris@gmail.com>
2015-11-13 15:42:25 -0800

    user model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,23 @@
 RSpec.describe User do
-	it "upates users high score" do
-		expect(user.hich_score).to eq 80
+	describe "user" do
+		let(:user) { User.new(high_score: 10) }
+
+		describe ".update_highscore" do
+			context "when argument > user's high score" do
+				it "updates the high score" do
+					user.update_highscore(20)
+
+					expect(user.high_score).to eq 20
+				end
+			end
+
+			context "when argument < user's high score" do
+				it "user's highscore is constant" do
+					user.update_highscore(5)
+
+					expect(user.high_score).to eq 10
+				end
+			end
+		end
 	end
 end
